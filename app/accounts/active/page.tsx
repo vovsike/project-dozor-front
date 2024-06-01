b8a742afbfe1c096d558633445be9eacefab01e5
Vladimir Garanin <vladimir.garanin2802@gmail.com>
2024-06-01 16:23:54 +0100

    Remove unused cookies import from active accounts page
    In the active accounts page file, an unused import statement for cookies has been removed. The update helps keep the code clean and efficient, reducing unnecessary line overhead.

--- app/accounts/active/page.tsx
+++ app/accounts/active/page.tsx
@@ -1,7 +1,6 @@
 import {Table, TableContainer, Tbody, Th, Thead, Tr} from "@chakra-ui/react";
 import {getAccountsActive} from "@/app/client";
 import AccountTableRow from "@/app/accounts/active/accountTableRow";
-import {cookies} from "next/headers";
 
 export default async function activeAccounts () {
 
