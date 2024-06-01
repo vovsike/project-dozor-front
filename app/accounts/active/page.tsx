ff08f26a15e4f8de513adf1be01a1a9c95a59fb6
Vladimir Garanin <vladimir.garanin2802@gmail.com>
2024-06-01 22:36:15 +0100

    Add registration link to active accounts page
    The active accounts page has been enhanced with the addition of a registration link. This new link redirects to the registration page, providing a direct path for users to create a new account if they want.

--- app/accounts/active/page.tsx
+++ app/accounts/active/page.tsx
@@ -1,25 +1,32 @@
 import {Table, TableContainer, Tbody, Th, Thead, Tr} from "@chakra-ui/react";
 import {getAccountsActive} from "@/app/client";
 import AccountTableRow from "@/app/accounts/active/accountTableRow";
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
 
 export default async function activeAccounts () {
 
     const accounts = await getAccountsActive();
 
     return (
-        <TableContainer>
-            <Table>
-                <Thead>
-                    <Tr>
-                        <Th>User name</Th>
-                        <Th>Email</Th>
-                        <Th>Action</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    <AccountTableRow accounts={accounts.data} />
-                </Tbody>
-            </Table>
-        </TableContainer>
+        <>
+            <TableContainer>
+                <Table>
+                    <Thead>
+                        <Tr>
+                            <Th>User name</Th>
+                            <Th>Email</Th>
+                            <Th>Action</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <AccountTableRow accounts={accounts.data} />
+                    </Tbody>
+                </Table>
+            </TableContainer>
+            <Link p={"5px"} as={NextLink} href={"/register"} >Or register an account</Link>
+
+        </>
+
     );
 }
